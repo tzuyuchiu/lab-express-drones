a8054b66433cec4f3db24a8c4c77e41277e36810
Tzu Yu Chiu <tzuyujulia@gmail.com>
2022-06-03 17:32:06 +0200

    done

--- routes/drones.js
+++ routes/drones.js
@@ -1,26 +1,63 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 // require the Drone model here
+const Drone = require('../models/Drone.model.js');
 
-router.get('/drones', (req, res, next) => {
+router.get('/drones', async (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
-})
+  try {
+    const findListOfDrone = await Drone.find();
+    res.status(200).json(findListOfDrone);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/drones', (req, res, next) => {
+router.post('/drones', async (req, res) => {
   // Iteration #3: Add a new drone
   // ... your code here
-})
+  const drone = req.body;
+  const addOneDrone = await Drone.create(drone);
 
-router.post('/drones/:id', (req, res, next) => {
+  if (!drone.name) {
+    res.status(400).json({
+      message: 'Drone name not provided',
+    });
+    return;
+  }
+
+  res.status(201).json({
+    message: 'received',
+  });
+});
+
+router.post('/drones/:id', async (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
-})
+  try {
+    const droneId = req.params.id;
+    // the "{new: true}" option allows us to get the updated version of the document instead of the previous one
+    const updatedDroneId = await Drone.findByIdAndUpdate(droneId, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedDroneId);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.delete('/drones/:id', (req, res, next) => {
+router.delete('/drones/:id', async (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
-})
+  try {
+    const deletedThing = await Drone.findByIdAndDelete(req.params.id);
+    console.log(deletedThing);
+    res.json({ message: `I deleted ${deletedThing.id}` });
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,31 @@
 // Iteration #1
+
+const ConnetDB = require('../db/index');
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model.js');
+
+const drones = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+];
+
+async function seedDatabase() {
+  try {
+    // console.log('test seedDB function');
+    // ! Delete everything from the database
+
+    await Drone.deleteMany();
+
+    // ! Create the new data
+    const createdDrones = await Drone.create(drones);
+    console.log(`Created ${createdDrones.length} drones`);
+    // ! Disconnect
+    await mongoose.disconnect();
+    // We can use process.exit() as well to quit the process.
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+seedDatabase();
