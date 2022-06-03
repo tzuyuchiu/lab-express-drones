15a623476e76e60d8626d72b731d7c39fa56c395
Tzu Yu Chiu <tzuyujulia@gmail.com>
2022-06-03 17:34:11 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 /* GET /
 
 It's useful to have a route which we can request to check the API is running.
 Sometimes this is at /health or /check.
 */
-router.get('/', (req, res, next) => res.json({ success: true }))
+router.get('/', (req, res, next) => res.json({ success: true }));
 
-module.exports = router
+module.exports = router;
